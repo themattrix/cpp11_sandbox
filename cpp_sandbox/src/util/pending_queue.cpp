85ff9b36480171f54edfb1800afb07d845fe9fd3
Matthew Tardiff <mattrix@gmail.com>
2012-03-19 02:12:28 -0500

    Changed macros into functions.

--- cpp_sandbox/src/util/pending_queue.cpp
+++ cpp_sandbox/src/util/pending_queue.cpp
@@ -27,41 +27,51 @@
 
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 ///                                                                                                                                               ///
-///                                                                   Macros                                                                      ///
-///                                                                                                                                               ///
-/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-#define  ensure(condition, description) \
-         [&]{bool r{condition}; current_results.emplace_back(description, r); if(!r && current_status) current_status = r;}()
-
-// ensure that the contents of two primitive-type containers are equal
-#define  ensure_equal_containers(a, b, description) \
-         ensure(a.size() == b.size() && std::equal(a.begin(), a.end(), b.begin()), description)
-
-// ensure that the contents of two string containers are equal
-#define  ensure_equal_string_containers(a, b, description) \
-         ensure(a.size() == b.size() && std::equal \
-               (a.begin(), a.end(), b.begin(), [](std::string const &x, std::string const &y){return (x.compare(y) == 0);}), description)
-
-
-/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-///                                                                                                                                               ///
-///                                                              Static Variables                                                                 ///
-///                                                                                                                                               ///
-/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-// records the status of the tests run so far
-static bool current_status = true;
-
-// records the current results
-static std::list<mdt::test::result> current_results;
-
-
-/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-///                                                                                                                                               ///
 ///                                                               Test Namespaces                                                                 ///
 ///                                                                                                                                               ///
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////////////////////////// static variables and helper functions ///
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+namespace mdt { namespace test
+{
+   // records the status of the tests run so far
+   static bool current_status = true;
+
+   // records the current results
+   static std::list<mdt::test::result> current_results;
+
+   // set a failure result
+   static void failure(std::string const &description)
+   {
+      current_results.emplace_back(description, false);
+
+      current_status = false;
+   }
+
+   // set a success result
+   static void success(std::string const &description)
+   {
+      current_results.emplace_back(description, true);
+   }
+
+   // if condition is true, add a 'success' result, otherwise add a 'failure'
+   static void ensure(bool condition, std::string const &description)
+   {
+      if(condition) success(description);
+      else          failure(description);
+   }
+
+   // ensure that the contents of two containers are equal
+   template<typename Ta, typename Tb>
+   static void ensure_equal_containers(Ta const &a, Tb const &b, std::string const &description)
+   {
+      ensure(a.size() == b.size() && std::equal(a.begin(), a.end(), b.begin()), description);
+   }
+}}
+
 
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////// pending_queue<std::string> tests ///
@@ -89,7 +99,7 @@ namespace mdt { namespace test { namespace pending_queue { namespace strings
       ensure(output.empty(), "strings: asynchronous call-back");
 
       // ensure that the input has not changed contents
-      ensure_equal_string_containers(input, INPUT, "strings: add from a container doesn't change the container");
+      ensure_equal_containers(input, INPUT, "strings: add from a container doesn't change the container");
 
       {
          // start the queue thread
@@ -104,7 +114,7 @@ namespace mdt { namespace test { namespace pending_queue { namespace strings
             q.sync();
 
             // ensure that the output is now equal to the input
-            ensure_equal_string_containers(input, output, "strings: add -> start thread -> sync = flushed output");
+            ensure_equal_containers(input, output, "strings: add -> start thread -> sync = flushed output");
 
             // clear the output for the next test
             output.clear();
@@ -122,7 +132,7 @@ namespace mdt { namespace test { namespace pending_queue { namespace strings
             q.sync();
 
             // ensure that the output is again equal to the input
-            ensure_equal_string_containers(input, output, "strings: start thread -> add -> sync = flushed output");
+            ensure_equal_containers(input, output, "strings: start thread -> add -> sync = flushed output");
 
             // clear the output for the next test
             output.clear();
@@ -149,7 +159,7 @@ namespace mdt { namespace test { namespace pending_queue { namespace strings
             q.sync();
 
             // ensure that the output is again equal to the input
-            ensure_equal_string_containers(input, output, "strings: start thread -> pause -> add -> un-pause -> sync = flushed output");
+            ensure_equal_containers(input, output, "strings: start thread -> pause -> add -> un-pause -> sync = flushed output");
 
             // clear the output for the next test
             output.clear();
@@ -170,19 +180,18 @@ namespace mdt { namespace test { namespace pending_queue { namespace strings
       }
 
       // ensure that the output is again equal to the input
-      ensure_equal_string_containers(input, output, "strings: start thread -> add -> pause -> stop thread = flushed output");
+      ensure_equal_containers(input, output, "strings: start thread -> add -> pause -> stop thread = flushed output");
 
       // clear the output for the next test
       output.clear();
 
+      /**
+       ** (5) Ensure that re-starting the queue thread from the same parent thread works, and that adding elements, not synchronizing,
+       **     and stopping the thread results in the elements being processed.
+       **/
       {
          // start the queue thread
          local(q.go());
-
-         /**
-          ** (5) Ensure that re-starting the queue thread from the same parent thread works, and that adding elements, not synchronizing,
-          **     and stopping the thread results in the elements being processed.
-          **/
 
          // add some more stuff
          for(auto i : input) q.add(i);
@@ -191,7 +200,29 @@ namespace mdt { namespace test { namespace pending_queue { namespace strings
       }
 
       // ensure that the output is now equal to the input
-      ensure_equal_string_containers(input, output, "strings: restart thread -> add -> stop thread = flushed output");
+      ensure_equal_containers(input, output, "strings: restart thread -> add -> stop thread = flushed output");
+
+      // clear the output for the next test
+      output.clear();
+
+      /**
+       ** (6) Ensure that stopped queues throw back the inserted element.
+       **/
+      try
+      {
+         // attempt to add some more stuff
+         for(auto i : input) q.add(i);
+
+         failure("strings: adding to a stopped queue should have thrown an exception");
+      }
+      catch(std::string const &e)
+      {
+         ensure(e == input[0], "strings: adding to a stopped queue should throw the first element added after stopping");
+      }
+      catch(...)
+      {
+         failure("strings: caught wrong exception type (should be 'std::string')");
+      }
    }
 }}}}
 
@@ -224,107 +255,40 @@ namespace mdt { namespace test { namespace pending_queue { namespace ints
       // ensure that the input has not changed contents
       ensure_equal_containers(input, INPUT, "integers: adding from a container doesn't change the container");
 
+      /**
+       ** (1) Ensure that starting and stopping the queue thread acts like a synchronization.
+       **/
       {
          // start the queue thread
          local(q.go());
-
-         /**
-          ** (1) Ensure that adding stuff to the pending queue before the queue's thread is started, then starting and synchronizing the queue,
-          **     results in the output queue being equal to the input queue.
-          **/
-         {
-            // wait until all elements have been processed
-            q.sync();
-
-            // ensure that the output is now equal to the input
-            ensure_equal_containers(input, output, "integers: add -> start thread -> sync = flushed output");
-
-            // clear the output for the next test
-            output.clear();
-         }
-
-         /**
-          ** (2) Ensure that adding stuff to the pending queue after the queue's thread is started, then synchronizing the queue, results in the
-          **     output queue being equal to the input queue.
-          **/
-         {
-            // add some more stuff while not paused and while thread is running
-            for(auto i : input) q.add(i);
-
-            // wait until all elements have been processed
-            q.sync();
-
-            // ensure that the output is again equal to the input
-            ensure_equal_containers(input, output, "integers: start thread -> add -> sync = flushed output");
-
-            // clear the output for the next test
-            output.clear();
-         }
-
-         /**
-          ** (3) Ensure that adding stuff to the pending queue while the queue is paused, then un-pausing and synchronizing the queue, results in
-          **     the output queue being equal to the input queue.
-          **/
-         {
-            // pause the queue
-            q.pause();
-
-            // add some more stuff while paused
-            for(auto i : input) q.add(i);
-
-            // ensure that the output queue has not had anything added to it yet
-            ensure(output.empty(), "integers: start thread -> pause -> add = no output");
-
-            // un-pause the queue
-            q.pause(false);
-
-            // wait until all elements have been processed
-            q.sync();
-
-            // ensure that the output is again equal to the input
-            ensure_equal_containers(input, output, "integers: start thread -> pause -> add -> un-pause -> sync = flushed output");
-
-            // clear the output for the next test
-            output.clear();
-         }
-
-         /**
-          ** (4) Ensure that stopping a paused queue while elements are still present in it results in the elements being processed.
-          **/
-         {
-            // pause the queue
-            q.pause();
-
-            // add some more stuff while paused
-            for(auto i : input) q.add(i);
-         }
-
-         // end the queue thread
-      }
-
-      // ensure that the output is again equal to the input
-      ensure_equal_containers(input, output, "integers: start thread -> add -> pause -> stop thread = flushed output");
-
-      // clear the output for the next test
-      output.clear();
-
-      {
-         // start the queue thread
-         local(q.go());
-
-         /**
-          ** (5) Ensure that re-starting the queue thread from the same parent thread works, and that adding elements, not synchronizing,
-          **     and stopping the thread results in the elements being processed.
-          **/
-
-         // add some more stuff
-         for(auto i : input) q.add(i);
 
          // end the queue thread
       }
 
       // ensure that the output is now equal to the input
       ensure_equal_containers(input, output, "integers: restart thread -> add -> stop thread = flushed output");
+
+      // clear the output for the next test
+      output.clear();
+
+      /**
+       ** (2) Ensure that stopped queues throw back the inserted element.
+       **/
+      try
+      {
+         // attempt to add some more stuff
+         for(auto i : input) q.add(i);
+
+         failure("integers: adding to a stopped queue should have thrown an exception");
+      }
+      catch(int e)
+      {
+         ensure(e == input[0], "integers: adding to a stopped queue should throw the first element added after stopping");
+      }
+      catch(...)
+      {
+         failure("integers: caught wrong exception type (should be 'int')");
+      }
    }
 }}}}
 
